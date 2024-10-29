a0b3489833de3c2689188f64eb381371f847f66b
stradimarius <mariusclement68@gmail.com>
2024-10-29 16:12:08 +0100

    start React

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
+const PRODUCTS = [
+  {category: "Fruits", price: "1$", stocked: true, name:"Apple"},
+  {category: "Fruits", price: "5$", stocked: true, name:"DragonFruit"},
+  {category: "Fruits", price: "7$", stocked: true, name:"PassionFruit"},
+  {category: "Fruits", price: "6$", stocked: true, name:"Cherry"},
+  {category: "Vegetables", price: "4$", stocked: true, name:"Spinach"},
+  {category: "Vegetables", price: "12$", stocked: true, name:"Pumpkin"},
+  {category: "Vegetables", price: "2$", stocked: true, name:"Peas"}
+]
 
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(new FormData(e.target));
-    
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="firstname"
-      />
-      <button>Envoyer</button>
-    </form>
-  );
+  
 }
 
 export default App;
