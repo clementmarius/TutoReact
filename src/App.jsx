98adcbcc1c4b51ce919a4124db65991fe5efd46e
stradimarius <mariusclement68@gmail.com>
2024-10-29 15:30:18 +0100

    start React

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const increment = () => {
-    setCount(count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
   };
 
   return (
