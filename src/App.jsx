8d49ec1fcf5a3d4de04bdc177e24c4e3e898149c
stradimarius <mariusclement68@gmail.com>
2024-10-29 14:37:43 +0100

    start React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 const title = "Bonjour les gens";
 const style = { color: "red", background: "yellow" };
 const showTitle = true;
+const todos = [
+  'Acheter de la farine',
+  'Preparer la pate',
+  'Faire les crepes'
+]
 
 function App() {
   const handleClick = () => {
@@ -9,11 +14,7 @@ function App() {
 
   return (
     <>
-      {showTitle ? 
-        <h1 id="title" className="title" style={style}>
-          {title}
-        </h1> : <p>demo</p>
-      }
+      <Title /> 
       <input type="text" />
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
@@ -21,8 +22,15 @@ function App() {
         doloribus eos praesentium ex rem non suscipit alias inventore,
         consequatur rerum recusandae sit labore?
       </p>
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))}
+      </ul>
     </>
   );
+}
+
+function Title () {
+  return <h1>Bonjour les gens</h1>
 }
 
 export default App;
