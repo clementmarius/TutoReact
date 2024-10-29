a20bbf96df6ed16ece4ad444dc1df92e1616b7c3
stradimarius <mariusclement68@gmail.com>
2024-10-29 16:25:05 +0100

    start React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 const PRODUCTS = [
-  {category: "Fruits", price: "1$", stocked: true, name:"Apple"},
-  {category: "Fruits", price: "5$", stocked: true, name:"DragonFruit"},
-  {category: "Fruits", price: "7$", stocked: true, name:"PassionFruit"},
-  {category: "Fruits", price: "6$", stocked: true, name:"Cherry"},
-  {category: "Vegetables", price: "4$", stocked: true, name:"Spinach"},
-  {category: "Vegetables", price: "12$", stocked: true, name:"Pumpkin"},
-  {category: "Vegetables", price: "2$", stocked: true, name:"Peas"}
-]
+  { category: "Fruits", price: "1$", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "5$", stocked: true, name: "DragonFruit" },
+  { category: "Fruits", price: "7$", stocked: true, name: "PassionFruit" },
+  { category: "Fruits", price: "6$", stocked: true, name: "Cherry" },
+  { category: "Vegetables", price: "4$", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "12$", stocked: true, name: "Pumpkin" },
+  { category: "Vegetables", price: "2$", stocked: true, name: "Peas" },
+];
 
 function App() {
-  
+  return (
+    <div className="container my-3">
+      <SearchBar />
+    </div>
+  );
+}
+
+function SearchBar() {
+  return (
+    <div>
+      <div className="mb-3">
+        <input value="" onChange={() => null} placeholder="Search..." />
+        <Checkbox
+          checked={false}
+          onChange={() => null}
+          label="Show available products"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/forms/Checkbox.jsx
+++ src/components/forms/Checkbox.jsx
@@ -1,7 +1,13 @@
 export function Checkbox({ checked, onChange, label }) {
   return (
     <div>
-      <input type="checkbox" className="form-check-input" checked={checked}/>
+      <input
+        type="checkbox"
+        className="form-check-input"
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+      <label className="form-check-label">{label}</label>
     </div>
   );
 }
