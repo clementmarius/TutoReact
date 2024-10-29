63d7c0cb613e4eaed3b1891738698db8735f3676
stradimarius <mariusclement68@gmail.com>
2024-10-29 14:27:26 +0100

    start React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+const title = "Bonjour les gens";
+const style = { color: "red", background: "yellow" };
+const showTitle = true;
+
 function App() {
-  return <h1>Bonjour les gens</h1>
+  const handleClick = () => {
+    alert("J'ai clique sur le titre");
+  };
+
+  return (
+    <>
+      {showTitle ? 
+        <h1 id="title" className="title" style={style}>
+          {title}
+        </h1> : <p>demo</p>
+      }
+      <input type="text" />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        quae voluptatem earum exercitationem necessitatibus, itaque, quo
+        doloribus eos praesentium ex rem non suscipit alias inventore,
+        consequatur rerum recusandae sit labore?
+      </p>
+    </>
+  );
 }
 
 export default App;
